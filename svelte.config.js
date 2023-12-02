ea3501ba208c0430cd93ed90f59716d477f339c0
sikutu <sikutu.hiro@gmail.com>
2023-12-02 22:40:11 +0900

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,31 @@
+/*import adapter from '@sveltejs/adapter-static';
+
+export default {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'docs',
+			assets: 'docs',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
+};
+
+import adapter from '@sveltejs/adapter-static';
+*/
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('docs') ? '' : process.env.BASE_PATH
+		}
+	}
+};
+
+export default config;
